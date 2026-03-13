2ccf496ab69a1b5c30867336a81dffae96ed1c72
Makoto146273 <146273@student.uni.opole.pl>
2026-03-13 08:53:49 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод двух чисел
+        Console.Write("Введите первое число: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        // Арифметические операции
+        double sum = a + b;
+        double difference = a - b;
+        double product = a * b;
+        double quotient = a / b;
+
+        // Вывод результатов
+        Console.WriteLine($"\nСумма: {sum}");
+        Console.WriteLine($"Разность: {difference}");
+        Console.WriteLine($"Произведение: {product}");
+        Console.WriteLine($"Частное: {quotient}");
+    }
+}
